6418b2df251254078b31f31b1a80768cb4593f25
Alex <alex.gattefosse@gmail.com>
2022-02-03 16:32:21 +0100

    fix node env 🪜

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,56 +1,61 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import glsl from 'vite-plugin-glsl';
 import ifdefRollupPlugin from './ifdef/ifdefRollupPlugin';
 
 import rollupConfig from './rollup.config';
 
-console.log('🏓 Environnement :', process.env.NODE_ENV);
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	if (!process.env.NODE_ENV) process.env.NODE_ENV = mode;
 
-const debug = process.env.NODE_ENV
-	? process.env.NODE_ENV != 'production'
-	: true;
-const define = {
-	DEBUG: debug,
+	console.log('🏓 Environnement :', process.env.NODE_ENV);
+
+	const debug = process.env.NODE_ENV
+		? process.env.NODE_ENV != 'production'
+		: true;
+	const define = {
+		DEBUG: debug,
+	};
+
+	return defineConfig({
+		server: {
+			port: '8080',
+			https: false,
+			open: false,
+			host: true,
+		},
+
+		plugins: [glsl(), ifdefRollupPlugin(define)],
+
+		assetsInclude: ['**/*.glb', '**/*.gltf'],
+
+		resolve: {
+			alias: [
+				{
+					find: '@js',
+					replacement: '/src/js',
+				},
+				{
+					find: '@glsl',
+					replacement: '/src/glsl',
+				},
+				{
+					find: '@utils',
+					replacement: '/src/utils',
+				},
+				{
+					find: '@workers',
+					replacement: '/src/workers',
+				},
+				{
+					find: '@src',
+					replacement: '/src',
+				},
+				{
+					find: '@@',
+					replacement: '/*',
+				},
+			],
+		},
+	});
 };
-
-export default defineConfig({
-	server: {
-		port: '8080',
-		https: false,
-		open: false,
-		host: true,
-	},
-
-	plugins: [glsl(), ifdefRollupPlugin(define)],
-
-	assetsInclude: ['**/*.glb', '**/*.gltf'],
-
-	resolve: {
-		alias: [
-			{
-				find: '@js',
-				replacement: '/src/js',
-			},
-			{
-				find: '@glsl',
-				replacement: '/src/glsl',
-			},
-			{
-				find: '@utils',
-				replacement: '/src/utils',
-			},
-			{
-				find: '@workers',
-				replacement: '/src/workers',
-			},
-			{
-				find: '@src',
-				replacement: '/src',
-			},
-			{
-				find: '@@',
-				replacement: '/*',
-			},
-		],
-	},
-});
