c1b590af916a41e4dac66171e7151ad79c9b581e
Alex <alex.gattefosse@gmail.com>
2022-02-14 23:09:15 +0100

    Update rollup.config.js

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -3,7 +3,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
 	input: 'src/app.js',
 	output: {
-		file: 'app.js',
+		file: 'index.js',
 		format: 'cjs',
 	},
 	plugins: [nodeResolve()],
