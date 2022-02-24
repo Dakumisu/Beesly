71a014dd3a4d857e487c20d93328eae631f5dadf
Alex <alex.gattefosse@gmail.com>
2022-02-24 14:15:29 +0100

    test rollup handlebars

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,9 +1,19 @@
+var handlebars = require('rollup-plugin-handlebars-plus');
+
+import content from '../src/json/content.json';
+
 export default {
 	input: 'src/app.js',
 	output: {
 		file: 'app.js',
 		format: 'cjs',
 	},
+	plugins: [
+		handlebars({
+			reloadOnPartialChange: false,
+			context: content,
+		}),
+	],
 	treeshake: {
 		moduleSideEffects: false,
 		tryCatchDeoptimization: true,
