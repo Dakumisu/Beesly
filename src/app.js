c3dd1095c8e2c2b1395fb5a408b12911c7c10ec3
Alex <alex.gattefosse@gmail.com>
2022-03-10 15:07:25 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ import signal from 'signal-js';
 import Dom from '@dom/Dom';
 import { initWebgl } from '@webgl/Webgl';
 
-signal.once('domLoaded', () => {
+const dom = new Dom();
+
+signal.on('domLoaded', () => {
 	const webgl = initWebgl(dom.nodes.domElements.canvas);
 });
-const dom = new Dom();
 
 /// #if DEBUG
 console.log('debug mode 🔥');
