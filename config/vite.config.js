8bec4d75cf1b3d5b47ea4daa3a60110e74c09072
Alex <alex.gattefosse@gmail.com>
2022-03-16 22:52:37 +0100

    Update vite.config.js

--- config/vite.config.js
+++ config/vite.config.js
@@ -6,14 +6,9 @@ import ifdefRollupPlugin from './ifdef/ifdefRollupPlugin';
 import content from '../src/json/content.json';
 
 export default ({ mode }) => {
-	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-	if (!process.env.NODE_ENV) process.env.NODE_ENV = mode;
+	console.log('🏓 Environnement :', mode);
 
-	console.log('🏓 Environnement :', process.env.NODE_ENV);
-
-	const debug = process.env.NODE_ENV
-		? process.env.NODE_ENV != 'production'
-		: true;
+	const debug = mode ? mode != 'production' : true;
 	const define = {
 		DEBUG: debug,
 	};
